fdd250cf6eab4eae57400637d24ddf0930458705
skyewu0 <swu23@amherst.edu>
2021-11-18 21:52:35 -0500

    add combined shiny

---
+++ shiny app/app.R
@@ -0,0 +1,134 @@
+library(shiny)
+library(shinythemes)
+library(tidyverse)
+library(leaflet)
+library(sf)
+library(tigris) # geojoin
+library(htmlwidgets)
+library(shinythemes)
+
+## Import Data
+incarceration_trends
+county_shapefile
+rates_by_race
+
+## Widgets
+# For interactive maps widget:
+year_choices <- unique(incarceration_trends$year)
+state_options <- unique(rates_by_race$ID)
+
+## UI
+ui <- navbarPage(
+  title = "Incarceration Rates",
+  # Tab 1: Histogram
+    tabPanel(
+      title = "Histogram",
+      sidebarLayout(
+        sidebarPanel(
+          selectInput(
+            inputId = "histvar",
+            label = "Choose a year:",
+            choices = state_options,
+            selected = "wisconsin"),
+          
+        ),
+        mainPanel(plotOutput(outputId = "hist", height= 700))
+      )
+    ),
+  # Tab 2: Map
+  tabPanel(
+    title = "Maps",
+    sidebarLayout(
+      sidebarPanel(
+        selectInput(inputId = "yearInput",
+                    label = "Year:",
+                    choices = year_choices,
+                    selected = "2018",
+                    multiple = FALSE)),
+      #sliderInput(inputId = "yearInput", 
+      #label = "Year", 
+      #min = 1970, 
+      #max = 2018, 
+      #value = 1970, 
+      #step = 1,
+      #animate =
+      #animationOptions(interval = 300, loop = TRUE)),
+      mainPanel(leafletOutput("mymap", height= 600))
+    )
+  )
+)
+
+## Server
+server <- function(input, output) {
+  # Histogram
+  data_for_hist <- reactive({
+    data <- rates_by_race %>% filter(ID==input$histvar)
+  })
+  
+  output$hist <- renderPlot({
+    
+    ggplot(data= data_for_hist(), aes(x=race, y=rate)) +
+      geom_bar(stat="identity", width=0.5, fill = "#2c7fb8") +
+      labs(x = "Ethnicity",
+           y = "Rate of Incarceration (Prisoners per 100,000 residents",
+           title = "Rates of Incarceration By Ethnicity)") +
+      
+      theme(
+        plot.title = element_text(family = "serif",             
+                                  face = "bold",              
+                                  color = 1,             
+                                  size = 23),
+        plot.caption = element_text(size = 10),
+        axis.text.x = element_text(family = "serif",             
+                                   size = 12),
+        axis.text.y = element_text(family = "serif",             
+                                   size = 12),
+        axis.title.x = element_text(family = "serif",             
+                                    size = 16),
+        axis.title.y = element_text(family = "serif",             
+                                    size = 16))
+    
+  })
+  
+  # Maps
+  year_jail <- reactive({
+    filtered <- incarceration_trends %>%
+      filter(year == input$yearInput) 
+    
+    m <- left_join(county_shapefile, filtered, by = "fips")
+    return(m)
+    #geo_join(county_shapefile, filtered, 'fips', 'fips', how = "left")
+  })
+  
+  output$mymap <- renderLeaflet({
+  bins <- c(0, 100, 250, 500, 1000, 2500, 5000, 10000, 30000)
+  pal <- colorBin(palette = "OrRd", bins = bins, na.color = "#D3D3D3")
+  
+  year_jail() %>% 
+    leaflet() %>% 
+    addProviderTiles(provider = "CartoDB.Positron") %>%
+    setView(-95.7129, 37.0902, zoom = 3) %>%
+    addPolygons(fillColor = ~ pal(year_jail()$total_jail_pop_rate),
+                stroke = FALSE,
+                smoothFactor = .5,
+                opacity = 1,
+                fillOpacity = 0.7,
+                highlightOptions = highlightOptions(weight = 5,
+                                                    fillOpacity = 1,
+                                                    color = "black",
+                                                    bringToFront = TRUE),
+                popup = ~ paste0("County Name: ", county %>% str_to_title(), "<br>",
+                                 "State: ", state %>% str_to_title(), "<br>",
+                                 "Total Jail Population: ", total_jail_pop, "<br>",
+                                 "Total Jail-Population rate: ",
+                                 total_jail_pop_rate %>% round(2))) %>%
+    addLegend("bottomright",
+              pal = pal,
+              values = ~ total_jail_pop_rate,
+              title = "Total Jail Population Rate (per 100,000",
+              opacity = 0.7)
+})
+}
+
+##Shiny App
+shinyApp(ui = ui, server = server)
